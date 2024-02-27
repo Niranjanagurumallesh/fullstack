85511cc0b8d3d66455fbb9784ae21adff87f3c1d
Niranjanagurumallesh <117429565+Niranjanagurumallesh@users.noreply.github.com>
2024-02-27 11:46:54 +0530

    Add files via upload

---
+++ Day 1/src/pages/auth/Register.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './Register.css';
+import { Link } from 'react-router-dom';
+
+function RegisterPage() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    // Perform registration logic here, for demonstration purposes, just log the data
+    console.log("Username:", username);
+    console.log("Email:", email);
+    console.log("Password:", password);
+    console.log("Confirm Password:", confirmPassword);
+  };
+
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+
+        <input type="submit" value="Register" style={{marginBottom:"30px",width:"150px",marginLeft:"120px",marginTop:"30px"}} />
+      </form>
+    </div>
+  );
+}
+
+export default RegisterPage;
